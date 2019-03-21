02cbad6605b09438ea07cb9ced0de4dd94b04262
fancaixia <1562045201@qq.com>
2019-03-21 16:01:01 +0800

    修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,18 @@
 
 let  utils = require('../../utils/utils.js')
-
 Page({
   data: {
 
   },
   onLoad(){
-
     //定时器延迟  模拟查询数据库返回信息
     setTimeout(() => {
-      utils.openShadow(this, true)
+      utils.openShadow(this)
     }, 1000)
-
   },
   //点击模态框关闭
-  closeModal(){
-    utils.closeModal(this);
+  closeShadow(){
+    utils.closeShadow(this);
   },
 
 })
--- utils/utils.js
+++ utils/utils.js
@@ -45,28 +45,15 @@ function setnodes(text){
     }]
   }]
 }
-
-  //打开模态框
-  // 1.第一个参数：当前作用域  
-  // 2.第二个参数：布尔值控制模态框显示与隐藏
-  // 3.第三个参数为模态框提示信息  可不传
-const openShadow = (that, flag, text ="网络异常！请稍后重试")=>{
-    
-    if (flag){
+//显示模态框
+const openShadow = (that, text ="网络异常！请稍后重试")=>{
       that.setData({
         shadowFlag: true,
         nodes: setnodes(text),
       })
-    }else{
-      that.setData({
-        shadowFlag: false,
-        nodes: [],
-      })
-    }
-    
-  }
+}
 //关闭模态框
-const closeModal = (that)=>{
+const closeShadow = (that)=>{
     that.setData({
       shadowFlag: false,
       nodes: [],
@@ -75,5 +62,5 @@ const closeModal = (that)=>{
 
   module.exports = {
     openShadow,
-    closeModal,
+    closeShadow,
   }
